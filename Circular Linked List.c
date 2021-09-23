3eef81d84e56ccf724cc9f902501112a5c991633
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-09-23 22:29:08 +0530

    Create Circular Linked List.c

---
+++ Circular Linked List.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+void linkedListTraversal(struct node* ptr){
+    while(ptr!= NULL)
+    {   printf(" Element : %d\n",ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main(){
+    struct node*head;
+    struct node*second;
+    struct node*third;
+    struct node*fourth;
+
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third = (struct node*)malloc(sizeof(struct node));
+    fourth = (struct node*)malloc(sizeof(struct node));
+
+    //Link first and second nodes
+    head->data = 4;
+    head->next = second;
+
+    //Link second and third nodes
+    second->data = 3;
+    second->next = third;
+
+    // Link third and fourth nodes
+    third->data = 66;
+    third->next = fourth;
+
+    // Terminate the list at the fourth node
+    fourth->data = 1;
+    fourth->next = NULL;
+
+     linkedListTraversal(head);
+    return 0;
+}
