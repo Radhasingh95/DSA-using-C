1548a28286f54daa43287534804b6c4cc8c343fc
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-10-30 21:24:29 +0530

    Template with Multiple Parameters

---
+++ C++_Code/Templates_with_multiple_parameters.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+/*
+CLASS TEMPLATE WITH MULTIPLE PARAMETERS (ONE, TWO OR MORE THAN TWO)
+template <class T1, class T2...(COMMA SEPARATED)>
+class nameOfClass{
+    //body
+}
+*/
+template <class T1, class T2>
+class myClass{
+    public:
+    T1 data1;
+    T2 data2;
+    myClass(T1 a,T2 b){
+        data1 = a;
+        data2 = b;
+    }
+    void display()
+    {
+        cout<<this->data1<<endl<<this->data2;
+    }
+
+};
+int main(){
+    myClass <int,float> obj(1,1.8);
+    obj.display();
+    return 0;
+}
